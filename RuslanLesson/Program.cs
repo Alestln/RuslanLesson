919a78eee7e92bee9745f60cb8677e946a92dc8d
Alestln <itworkplace46@gmail.com>
2025-06-22 14:20:31 +0300

    Add project files.

---
+++ RuslanLesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RuslanLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
