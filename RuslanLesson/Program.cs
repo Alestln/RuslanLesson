26fa2be9b0f3d6809f43306d5a750d5c680f0892
Alestln <itworkplace46@gmail.com>
2025-12-06 16:18:20 +0200

    Done

--- RuslanLesson/Program.cs
+++ RuslanLesson/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            
+            // Булевый тип данных
+            // bool - логический тип данных, который может принимать два значения: true (истина) и false (ложь)
+            bool isTrue = true;
+
+            // Операторы сравнения
+            // > - больше
+            // < - меньше
+            // >= - больше или равно
+            // <= - меньше или равно
+            // == - равно. Не путать с присваиванием (=)!
+            // != - не равно
+
+            /*if (условие)
+            {
+                тело условия
+            }*/
+
+            Console.Write("Enter a first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"First number {firstNumber} greater second number {secondNumber}?");
+
+            if (firstNumber > secondNumber)
+            {
+                
+            }
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("First number are equal with second number.");
+            }
+
+            Console.WriteLine("End of program.");
         }
     }
 }
